91bded2f6ae815ce87ad2896e453b924188e6eee
Paul Reed <paul.reed@puppet.com>
2020-01-28 20:01:06 -0500

    test

--- lib/puppet/functions/pmp/get_account_details.rb
+++ lib/puppet/functions/pmp/get_account_details.rb
@@ -17,10 +17,10 @@ Puppet::Functions.create_function(:'pmp::get_account_details') do
 
         begin 
             gemData = PasswordManagerProGem.new(host,token,path,port)
-            result = gemData.getAccountPassword(resource,account)
+            geData.disable_ssl = true
+            result = gemData.getAccountDetails(resource,account)
         rescue => err
             result = err
-#        result = {"RESOURCE_ID":1,"RESOURCE_NAME":"Windows","RESOURCE_DESCRIPTION":"","OPERATINGSYSTEM":"Windows","DNSNAME":"","DEPARTMENT":"","LOCATION":"","RESOURCE_URL":""}
         end
     end
     result
